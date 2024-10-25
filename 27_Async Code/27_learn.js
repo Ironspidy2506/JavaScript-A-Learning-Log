68c6090d462c65d51bd27ae529d5197ed7c1aea0
Priyanshu Sahu <priyanshusahu085@gmail.com>
2024-10-26 01:40:38 +0530

    first commt

---
+++ 27_Async Code/27_learn.js
@@ -0,0 +1,16 @@
+// *************** Async Code ***************
+
+// By Default
+// JavaScript -> 1) Synchronous
+//               2) Single Threaded
+
+// Execution Context
+// Execution one line of code at a time
+// Each operation waits for the last one to complete before the execution
+
+// Blocking Code VS Non-Blocking Code
+// Blocking Code - Block the flow of Program (Read File Sync)
+// Non Blocking Code - Does not block execution (Read file Async)
+
+// Note : Depends on use-cases which code method to use
+
---
+++ 29_Promises/29_learn.js
@@ -0,0 +1,11 @@
+// *************** Promises ***************
+
+// Promises ka ye matlab hai ki aaapne jo task diya hai wo abhi ke abhi complete nai hoga, Queue mein lag gaya hai par abhi ke abhi usko load nai kiya gaya hai, complete nai kiya gaya hai
+
+
+/*
+Promise has 3 States
+1) Pending
+2) Fulfilled
+3) Rejected
+*/
